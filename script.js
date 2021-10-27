dfff8da371a2808e1158c4856dcac94e39e33e3d
Rosa Marquez <rmarquez101@cnm.edu>
2021-10-27 09:00:41 -0600

    snap challenge

---
+++ script.js
@@ -0,0 +1,9 @@
+let people = ['bob', 'jim', 'matt', 'mary', 'suzy', 'sarah'];
+people.forEach((people)=>[
+    console.log(people)
+]);
+
+let pTag = document.getElementById('list')
+people.forEach((people)=>[
+    console.log(people)
+])
